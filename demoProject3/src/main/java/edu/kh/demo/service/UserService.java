e831be3f96cbfe27276d5b9dcb5ceadd9d55b376
hho1204 <limho1288@gmail.com>
2024-09-06 17:27:14 +0900

    2024-09-06 <Spring> DemoProject 3 완성 및 TodoList 프로젝트 세팅

--- demoProject3/src/main/java/edu/kh/demo/service/UserService.java
+++ demoProject3/src/main/java/edu/kh/demo/service/UserService.java
@@ -1,5 +1,9 @@
 package edu.kh.demo.service;
 
+import java.util.List;
+
+import edu.kh.demo.dto.UserDto;
+
 public interface UserService {
 
 	/** 사용자 이름 조회
@@ -7,5 +11,34 @@ public interface UserService {
 	 * @return
 	 */
 	String selectUserName(int userNo);
+	
+	/** 사용자 전체 조회
+	 * @return userList
+	 */
+	List<UserDto> selectAll();
+	
+	
+	UserDto selectUser(int userNo);
+	
+	/** 사용자 정보 수정
+	 * @param user
+	 * @return result
+	 */
+	int updateUser(UserDto user);
+	
+	/** 사용자 정보 삭제
+	 * @param userNo
+	 * @return
+	 */
+	int deleteUser(int userNo);
+
+	/** 사용자 정보 추가
+	 * 
+	 * @param user
+	 * @return result
+	 */
+	int InsertUser(UserDto user);
+
+	
 	
 }
