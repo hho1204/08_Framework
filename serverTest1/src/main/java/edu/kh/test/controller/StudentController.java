4e729f594366cdd331a50623586a8718b2b6a750
hho1204 <limho1288@gmail.com>
2024-09-12 17:23:27 +0900

    2024-09-12  <서버프로그램 구현 및 인터페이스 구현> 시험 문제 풀이

---
+++ serverTest1/src/main/java/edu/kh/test/controller/StudentController.java
@@ -0,0 +1,22 @@
+package edu.kh.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import edu.kh.test.dto.Student;
+
+@Controller
+public class StudentController {
+	
+	
+	@PostMapping("/student/select")
+	public String selectStudent(Model model, @ModelAttribute Student student) {
+		model.addAttribute("stdName", student.getStdName());
+		model.addAttribute("stdAge", student.getStdAge());
+		model.addAttribute("stdAddress", student.getStdAddress());
+		return "student/select";
+	}
+	
+}
