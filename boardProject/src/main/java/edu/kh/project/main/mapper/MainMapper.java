23995833489314ca6be9e750c14a753a21f4e427
hho1204 <limho1288@gmail.com>
2024-09-26 17:50:57 +0900

    2024 09 26

---
+++ boardProject/src/main/java/edu/kh/project/main/mapper/MainMapper.java
@@ -0,0 +1,40 @@
+package edu.kh.project.main.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import edu.kh.project.member.dto.Member;
+import io.lettuce.core.dynamic.annotation.Param;
+
+@Mapper // 인터페이스 상속 클래스 생성 + 클래스를 Bean 등록
+public interface MainMapper {
+
+	/** 전체 회원 조회
+	 * @return list
+	 */
+	List<Member> selectMemberList();
+
+	/** 빠른 로그인
+	 * @param memberNo
+	 * @return loginMember
+	 */
+	Member directLogin(int memberNo);
+
+	/** 비밀번호 초기화
+	 * @param memberNo
+	 * @param encPw
+	 * @return result
+	 */
+	int resetPw(@Param("no") int memberNo, @Param("pw") String encPw);
+	
+	/** 회원 탈퇴 변경
+	 * 
+	 * @param memberNo
+	 * @return
+	 */
+	int changeStatus(int memberNo);
+
+	
+}
