dc511f1332f299f89db85b9529c1e3b19ba6007e
hho1204 <limho1288@gmail.com>
2024-10-08 16:36:04 +0900

    2024_10_08 <FrameWork> 게시글 좋아요 기능

--- boardProject/src/main/java/edu/kh/project/board/service/BoardService.java
+++ boardProject/src/main/java/edu/kh/project/board/service/BoardService.java
@@ -1,5 +1,6 @@
 package edu.kh.project.board.service;
 
+import java.util.List;
 import java.util.Map;
 
 import edu.kh.project.board.dto.Board;
@@ -18,5 +19,25 @@ public interface BoardService {
 	 * @return board
 	 */
 	Board selectDetail(Map<String, Integer> map);
+	
+	/** 조회수 1 증가
+  * 
+  * @param boardNo
+  * @return
+  */
+	int updateReadCount(int boardNo);
+	
+	/** 게시글 좋아요
+	 * @param boardNo
+	 * @param memberNo
+	 * @return map
+	 */
+	Map<String, Object> boardLike(int boardNo, int memberNo);
+	
+	/** DB에서 모든게시판 종류룰 조회
+	 * 
+	 * @return
+	 */
+	List<Map<String, String>> selectBoardTypeList();
 
 }
